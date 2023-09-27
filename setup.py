dc8b2c74db4ed52605a0f6e48ff9e556fc8553ca
Raj Narayanan <brajnarayanan.b@gmail.com>
2023-09-27 12:25:10 +0530

    flake8 code added in tox.ini

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "src",
-    version = "0.0.1",
-    description = "It's an MLOPS package for wine quality",
-    author = "Raj",
-    packages = find_packages(),
-    license = "MIT"
-    )
+    name="src",
+    version="0.0.1",
+    description="It's an MLOPS package for wine quality",
+    author="Raj",
+    packages=find_packages(),
+    license="MIT"
+)
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,17 +1,20 @@
 import pytest
 
+
 class NotInRange(Exception):
     def __init__(self, message="Value not in Range"):
-        self.message=message
+        self.message = message
         super().__init__(self.message)
 
+
 def test_generic():
-    a=5
+    a = 5
     with pytest.raises(NotInRange):
-        if a not in range(10,20):
+        if a not in range(10, 20):
             raise NotInRange
 
+
 def test_something():
-    a=5
-    b=5
-    assert a==b
+    a = 5
+    b = 5
+    assert a == b
