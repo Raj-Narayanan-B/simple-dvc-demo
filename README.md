52a4b3afce1917808fc2251cc2c68377cd81fb56
Raj Narayanan <brajnarayanan.b@gmail.com>
2023-10-02 22:53:24 +0530

    mlflow start

--- README.md
+++ README.md
@@ -59,3 +59,11 @@ Build your own package commands
 ```bash
 python setup.py sdist bdist_wheel
 ```
+
+MLFLOW server command:
+```bash
+mlflow server \
+    --backend-store-uri sqlite:///mlflow.db \
+    --default-artifact-root ./artifacts \
+    --host 127.0.0.1 -p 1234
+```
--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -11,17 +11,19 @@ import numpy as np
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import ElasticNet
-#from urllib.parse import urlparse
+from urllib.parse import urlparse
 from get_data import read_params
 import argparse
 import joblib
 import json
 
+import mlflow
+
 def eval_metrics(ytest,ypred):
     rmse=np.sqrt(mean_squared_error(ytest,ypred))
     mae=mean_absolute_error(ytest,ypred)
     r2=r2_score(ytest,ypred)
-    return(rmse,mae,r2)
+    return rmse,mae,r2
 
 def train_and_evaluate(config_path):
     config = read_params(config_path)
@@ -44,44 +46,49 @@ def train_and_evaluate(config_path):
     xtrain = train.drop(labels=target,axis=1)
     xtest = test.drop(labels=target,axis=1)
 
-    model=ElasticNet(alpha=alpha,l1_ratio=l1_ratio, random_state=random_state)
-    model.fit(xtrain,ytrain)
+################################## MLFLOW BEGINS ##################################
 
-    ypred=model.predict(xtest)
+    mlflow_config = config["mlflow_config"]
+    remote_server_uri = mlflow_config["remote_server_uri"]
 
-    (rmse,mae,r2)=eval_metrics(ytest,ypred)
+    mlflow.set_tracking_uri(remote_server_uri)
 
-    print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
-    print("  RMSE: %s" % rmse)
-    print("  MAE: %s" % mae)
-    print("  R2: %s" % r2)
+    mlflow.set_experiment(mlflow_config["experiment_name"])
 
-#####################################################
-    scores_file = config["reports"]["scores"]
-    params_file = config["reports"]["params"]
+    with mlflow.start_run(run_name=mlflow_config["run_name"]) as mlops_run:
+        model=ElasticNet(alpha=alpha,l1_ratio=l1_ratio, random_state=random_state)
+        model.fit(xtrain,ytrain)
 
-    with open(scores_file, "w") as f:
-        scores = {
-            "rmse": rmse,
-            "mae": mae,
-            "r2": r2
-        }
-        json.dump(scores, f, indent=4)
+        ypred=model.predict(xtest)
 
-    with open(params_file, "w") as f:
-        params = {
-            "alpha": alpha,
-            "l1_ratio": l1_ratio,
-        }
-        json.dump(params, f, indent=4)
-#####################################################
+        (rmse,mae,r2) = eval_metrics(ytest,ypred)
+
+        mlflow.log_param("alpha",alpha)
+        mlflow.log_param("l1_ratio",l1_ratio)
+
+        mlflow.log_metric("rmse", rmse) # type: ignore
+        mlflow.log_metric("mae", mae) # type: ignore
+        mlflow.log_metric("r2", r2) # type: ignore
 
 
-    os.makedirs(model_dir, exist_ok=True)
-    model_path = os.path.join(model_dir, "model.joblib")
+        # The traditional model logging method below is also going to be commented out.
+        # Instead, we will use mlflow.
+        # os.makedirs(model_dir, exist_ok=True)
+        # model_path = os.path.join(model_dir, "model.joblib")
 
-    joblib.dump(model, model_path)
+        # joblib.dump(model, model_path)
 
+        tracking_url_type_store = urlparse(mlflow.get_artifact_uri()).scheme
+        if tracking_url_type_store != "file":
+            mlflow.sklearn.log_model(model, 
+                                     "model", 
+                                     registered_model_name=mlflow_config["registered_model_name"]
+                                    )
+        else:
+            mlflow.sklearn.log_model(model, "model")
+
+
+################################## MLFLOW ENDS ##################################
 
 if __name__=="__main__":
     args = argparse.ArgumentParser()
